66b9567f8b2a56e2a881208c64df25fe0a552ebd
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-13 17:37:02 -0700

    Update VCR settings and add integration tests

---
+++ nsa_api/spec/controllers/internal_response_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe GeoApiController, type: :controller do
+ context "integration tests" do
+    describe "process api calls gracefully" do
+       it "should handle timeouts gracefully" do
+        get :index
+        response.status.should be_in([200, 408])
+      end
+
+      it "should be successful for 98% of queries" do
+        get :index
+        if response.status == 200
+          json = JSON.parse(response.body)
+          json["geo_api"].length.should be(100)
+        end
+      end
+    end
+
+    describe "Get user specific location data" do
+      it "should return a json object given a first and last name" do
+        get :show, {:first_name => "Rosamond", :last_name => "Tromp", ip: "50.250.223.177"}
+
+        if response.status == 200
+          json = JSON.parse(response.body)
+          json.length.should be(5)
+        else
+          response.status.should be(408)
+        end
+      end
+
+
+      it "should return a 404 status if unavailable" do
+        get :show, {:first_name => "Raghu", :last_name => "Reddy", ip: "50.250.223.177"}
+        response.status.should be(404)
+      end
+
+      it "should return an error if a user data is unavailable" do
+        get :show, {:first_name => "Raghu", :last_name => "Reddy", ip: "50.250.223.177"}
+        json = JSON.parse(response.body)
+        expect(json["error"]).to eq("No user location data available.")
+      end
+
+    end
+
+  end
+
+end
--- nsa_api/spec/support/vcr_setup.rb
+++ nsa_api/spec/support/vcr_setup.rb
@@ -1,10 +1,6 @@
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
   c.hook_into :faraday
-  c.before_http_request do |request|
-    VCR.insert_cassette('global', :record => :new_episodes)
-  end
-  c.after_http_request do |request|
-    VCR.eject_cassette
-  end
+
+  c.allow_http_connections_when_no_cassette = true
 end
