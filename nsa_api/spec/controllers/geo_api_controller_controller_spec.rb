21dd3679c600c4e85fc1ddb09a538f640996c26e
Raghu Reddy <raghureddy.ram@gmail.com>
2015-07-13 21:10:36 -0700

    Update coverage

--- nsa_api/spec/controllers/geo_api_controller_controller_spec.rb
+++ nsa_api/spec/controllers/geo_api_controller_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GeoApiController, type: :controller do
         it "gets a response from the blockscore api" do
           VCR.use_cassette 'controller/gov_api_response' do
             request = Faraday.get("https://gov.blockscore.com/api/people")
-            request.status.should be_in([200, 400])
+            request.status.should be_in([200, 408])
           end
         end
 
@@ -29,7 +29,6 @@ RSpec.describe GeoApiController, type: :controller do
           VCR.use_cassette('controller/all_internal_data') do
             request = Net::HTTP.get_response('localhost', '/', 3000).body
 
-            binding.pry
             expect(JSON.parse(request)["geo_api"].length).to be(100)
           end
         end
@@ -44,12 +43,24 @@ RSpec.describe GeoApiController, type: :controller do
 
       describe "get specific user object" do
         it "Gets a specific users object given params" do
-          VCR.use_cassette('controller/specific_internal_data', :record => :new_episodes) do
+          VCR.use_cassette('controller/specific_internal_data') do
             request = Net::HTTP.new('localhost', 3000).get('/location?last_name=Tromp&first_name=Rosamond')
             request = Net::HTTP.get_response('localhost', '/location?last_name=Tromp&first_name=Rosamond', 3000).body
 
             data = JSON.parse(request)
             expect(data["stated_distance_from_phone"]).to be_a(Float)
+          end
+        end
+      end 
+
+      describe "get specific user object" do
+        it "responds with the appropriate status" do
+          VCR.use_cassette('controller/specific_internal_data') do
+            request = Net::HTTP.new('localhost', 3000).get('/location?last_name=Tromp&first_name=Rosamond')
+            request = Net::HTTP.get_response('localhost', '/location?last_name=Tromp&first_name=Rosamond', 3000)
+
+            code = (request.code.to_i)
+            expect(code).to be_in([200, 408])
           end
         end
       end 
